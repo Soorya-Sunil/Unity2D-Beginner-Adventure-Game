85a0a4d17a93fa818700952e1568417939067a79
Soorya <sooryasunil009@gmail.com>
2026-02-11 19:10:14 +0400

    health & UI updates

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -5,14 +5,27 @@ using UnityEngine.UIElements;
 
 public class UIHandler : MonoBehaviour
 {
-    public float CurrentHealth = 0.5f;
+    private VisualElement m_Healthbar;
+    public static UIHandler instance { get; private set; }
+
+	// Awake is called when the script instance is being loaded (in this situation, when the game scene loads)
+	private void Awake()
+	{
+		instance = this;
+	}
 
 
-    // Start is called before the first frame update
-    void Start()
+	// Start is called before the first frame update
+	void Start()
     {
         UIDocument uiDocument = GetComponent<UIDocument>();
-        VisualElement healthBar = uiDocument.rootVisualElement.Q<VisualElement>("HealthBar");
-        healthBar.style.width = Length.Percent(CurrentHealth * 100.0f);
+        m_Healthbar = uiDocument.rootVisualElement.Q<VisualElement>("HealthBar");
+        SetHealthValue(1.0f);
     }
+
+
+    public void SetHealthValue(float percentage) 
+    {
+		m_Healthbar.style.width = Length.Percent(100 * percentage);
+	}
 }
